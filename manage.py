2956ed163e7248ba85922847652568803d6358eb
ismv-dev <ismaelgalindob95@gmail.com>
2025-07-06 23:45:00 -0400

    final

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cer3.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "municipalidad.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- municipalidad/wsgi.py
+++ municipalidad/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cer3 project.
+WSGI config for municipalidad project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cer3.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "municipalidad.settings")
 
 application = get_wsgi_application()
